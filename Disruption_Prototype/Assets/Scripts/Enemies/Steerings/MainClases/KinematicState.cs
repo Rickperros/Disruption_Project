47fe27294227502671adef5600efe3068c4af415
Rickperros <ricky.perea@gmail.com>
2018-02-01 19:06:37 +0100

    Steerings Behaivour
    Starting to implement steerings

---
+++ Disruption_Prototype/Assets/Scripts/Enemies/Steerings/MainClases/KinematicState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class KinematicState : MonoBehaviour
+{
+    #region Constraints
+
+    public float m_maxLinearAcceleration = 0f;
+    [Tooltip("In Degrees")]
+    public float m_maxAngularAcceleration = 0f;
+
+    public float m_maxLinearSpeed = 0f;
+    public float m_maxAngularSpeed = 0f;
+
+    #endregion
+
+    #region Kinematic state
+
+    public float m_orientation = 0f;
+    public float m_angularSpeed = 0f;
+    public Vector3 m_position = Vector3.zero;
+    public Vector3 m_linearVelocity = Vector3.zero;
+
+    #endregion
+}
---
+++ Disruption_Prototype/Assets/Scripts/Enemies/Steerings/MainClases/SteeringOutput.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class SteeringOutput
+{
+    public bool m_linearActive = true;
+    public bool m_angularActive = false;
+
+    public float m_angularAcceleration = 0f;
+    public Vector3 m_linearAcceleration = Vector3.zero;
+}
